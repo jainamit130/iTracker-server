aea1d2818b93cde1b12152be054a651964a233bc
Indranil16 <indranil.banik@accolitedigital.com>
2022-03-22 00:58:04 +0530

    Removed all files

--- src/main/java/com/accolitedigital/iTracker/service/RecruiterService.java
+++
@@ -1,8 +0,0 @@
-package com.accolitedigital.iTracker.service;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-@Service
-public class RecruiterService {
-}
